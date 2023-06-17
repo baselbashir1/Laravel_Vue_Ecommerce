8560a4216c09877778615457858304cb50d7d791
Basel Bashir <eng.baselbashir@gmail.com>
2023-06-18 00:49:50 +0300

    Update ProductFactory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -19,25 +18,15 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
-        // return [
-        //     'title' => fake()->text(),
-        //     'image' => fake()->imageUrl(),
-        //     'description' => fake()->realText(2000),
-        //     'price' => fake()->randomFloat(2, 2, 5),
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        //     'created_by' => 1,
-        //     'updated_by' => 1,
-        // ];
-
         $files = Storage::files('public/images');
         $randomFile = Arr::random($files);
         $imageUrl = Storage::url($randomFile);
 
         return [
-            'title' => 'HyperX Cloud II - Gaming Headset',
+            'title' => 'HyperX - ' . fake()->text(100),
             'image' => $imageUrl,
-            'price' => '69.00',
+            'description' => fake()->realText(200),
+            'price' => fake()->randomFloat(2, 50, 500),
             'created_at' => now(),
             'updated_at' => now(),
             'created_by' => 1,
